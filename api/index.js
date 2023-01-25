e277e3a17ca54da97a4f3c2287af6fb90069a224
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-01-25 11:09:10 -0300

    ready back

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {  // alter para no borrar a cada rato
+conn.sync({ force: true }).then(() => {  // alter para no borrar a cada rato
   server.listen(3001, () => {
     console.log(`server raised in port ${port}`); // eslint-disable-line no-console
   });
