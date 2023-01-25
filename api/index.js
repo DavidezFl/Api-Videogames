59b8c80196189e90bc4dbe8f4ff0bfd69668e305
D4v1D <92929147+DavidezFl@users.noreply.github.com>
2023-01-25 10:52:40 -0300

    deploy back

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
@@ -6,6 +6,6 @@ const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {  // alter para no borrar a cada rato
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`server raised in port ${port}`); // eslint-disable-line no-console
   });
 });
